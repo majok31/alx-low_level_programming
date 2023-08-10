ad660cae5529b14eddbe1707dbf14e4300386822
majok <majoktot70@gmail.com>
2023-08-10 13:36:59 -0700

    write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <string.h>
 /**
  * main - main function
- * write - to output the message to standard error
+ * write - to output to standard error
  * Return: return 1 to indicate error
 */
 int main(void)
 {
-char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 write(2, message, strlen(message));
 return (1);
 }
